012f8fe2fa169d0b92fc5170ef35310723d3a8e1
ThiDK <dangkimthi84@gmail.com>
2018-11-26 13:52:22 +0700

    FPT University 2
    fpt university 2

--- FAT 2018/SEM3/2 WAD/DemoFull/Session05/FPTUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ FAT 2018/SEM3/2 WAD/DemoFull/Session05/FPTUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -7,10 +7,11 @@ namespace ContosoUniversity.DAL
     public class SchoolContext : DbContext
     {
         public DbSet<Course> Courses { get; set; }
-        public DbSet<Department> Departments { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Instructor> Instructors { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         public DbSet<Person> People { get; set; }
 
