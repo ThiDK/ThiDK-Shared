8c60268d77771f9e6896b62029ce6052735c2612
ThiDK <dangkimthi84@gmail.com>
2018-12-28 12:12:29 +0700

    Customer Manager
    version 2: WCF, MVC, UWP

---
+++ FAT 2018/SEM3/3 EAP/Demo/CusSystem/CustomerMVC/CustomerMVC/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+            return View(service.GetCustomers());
+        }
+    }
+}
---
+++ FAT 2018/SEM3/3 EAP/Demo/CusSystem/CustomerUWP/CustomerUWP/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CustomerUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ServiceReference1.Service1Client client;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            client = new ServiceReference1.Service1Client();
+            DisplayCustomer();
+        }
+
+        private async void DisplayCustomer()
+        {
+            GridViewCustomer.ItemsSource = await client.GetCustomersAsync();
+        }
+      
+    }
+}
