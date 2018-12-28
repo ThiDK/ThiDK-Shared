5a656e7b484f3f8134ac37370f42975256f10597
ThiDK <dangkimthi84@gmail.com>
2018-12-28 10:15:57 +0700

    WCF Services
    Customer Manager (wcf, mvc, upw)

---
+++ FAT 2018/SEM3/3 EAP/Demo/Demo2/ProductManager/ProductManager/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ProductManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductManager.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Apple iPhone Xs Max 256GB Bạc", Category = "iPhone", Price = 34.99M },
+            new Product { Id = 2, Name = "Apple iPhone Xs Max 512GB Vàng kim", Category = "iPhone", Price = 37.99M },
+            new Product { Id = 3, Name = "Apple iPhone Xs 64GB Xám đen", Category = "iPhone", Price = 24.49M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ FAT 2018/SEM3/3 EAP/Demo/Demo3/CustomerClient/CusApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CusApp.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CusApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        private Service1Client client;
+        public  MainPage()
+        {
+            this.InitializeComponent();
+
+            client = new Service1Client();
+            DisplayCustomer();
+
+        }
+
+        private async void DisplayCustomer()
+        {
+            grvCustomer.ItemsSource = await client.GetCustomersAsync();
+        }
+
+    }
+}
